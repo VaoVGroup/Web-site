9998b72930e88cb7207616de8f22e770370faa48
Sait Ramazan <70922350+srgul@users.noreply.github.com>
2021-02-11 20:51:29 +0300

    committed

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="content">
+        <div class="-button col-md-12"></div>
+        <div class="mid-button"></div>
+        <div class="bottom-button"></div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+  
+    
+</style>
